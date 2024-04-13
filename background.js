eb6889903d7bf24e068332fb4731cbae713b373f
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-04-14 08:14:12 +0900

    フォーカスが当たっているtextareaの![icon](xxxxx)の形式の文字列を見つけて、<img>タグに置き換えるようにしたら動いた

--- background.js
+++ background.js
@@ -7,28 +7,17 @@ chrome.runtime.onInstalled.addListener(() => {
 })
 
 function replaceImage() {
-  let selection = window.getSelection();
-  if (!selection.rangeCount) return;
-  let range = selection.getRangeAt(0);
-
-  let selectionText = selection.toString();
-  let regex = /\((.*?)\)/;
-  let match = selectionText.match(regex);
+  let textarea = document.activeElement;
+  if (textarea.tagName !== 'TEXTAREA') return;
+  let textAreaText = textarea.value;
+  let regex = /!\[icon\]\((.*?)\)/;
+  let match = textAreaText.match(regex);
   if (match) {
     let url = match[1];
-    let imgTagText = `<img width=500 src="${url}">`; 
-    console.log(imgTagText);
-    range.deleteContents();
-    let textNode = document.createTextNode(imgTagText);
-    range.insertNode(textNode);
+    console.log(url);
+    let imgTagText = `<img width=500 src="${url}">`;
+    textarea.value = textarea.value.replace(regex, imgTagText);
   }
-  // if (!selection.rangeCount) return;
-
-  // let range = selection.getRangeAt(0);
-  // let imgTag = document.createElement('img');
-  // imgTag.src = selection.toString();
-  // range.deleteContents();  // 選択範囲の内容を削除
-  // range.insertNode(imgTag);  // 選択範囲に<img>タグを挿入
 }
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
